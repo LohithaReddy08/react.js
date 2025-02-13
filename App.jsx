f9945cf9c8c3fd1df30060e2bf77e22fd1e3afbf
LohithaReddy08 <lohithasyamala2004@gmail.com>
2025-02-13 13:03:33 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import {BrowserRouter  , Routes , Route} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Clasroom from './pages/Clasroom';
+import Assessments from './pages/Assessments';
+import Store from './pages/Store';
+import Calender from './pages/Calender';
+import News from './pages/News';
+import Blog from './pages/Blog';
+import Profile from './pages/Profile';
+import MyAccount from './pages/MyAccount';
+import MySettings from './pages/MySettings';
+import PageNotFound from './pages/PageNotFound';
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/classroom' element={<Clasroom/>}/>
+            <Route path='/assessment' element={<Assessments/>}/>
+            <Route path='/store' element={<Store/>}/>
+            <Route path='/calender' element={<Calender/>}/>
+            <Route path='/news' element={<News/>}/>
+            <Route path='/blog' element={<Blog/>}/>
+            <Route path='/Profile' element={<Profile/>}/>
+            <Route path='/Profile/myaccount' element={<MyAccount/>}/>
+            <Route path='/Profile/mysettings' element={<MySettings/>}/>
+            <Route path='*' element={<PageNotFound/>}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
